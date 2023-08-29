5aa49611c64915e3176a9bfb05a6b8d090d90fae
BenjaminChandler1 <chandlerb566@gmail.com>
2023-08-29 15:44:06 -0400

    new places can be added

--- views/controller/places.js
+++ views/controller/places.js
@@ -1,4 +1,24 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
+
+router.get('/', (req, res) => {
+    res.render('places/index', { places })
+})
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
 
 // GET /places
 app.get('/', (req, res) => {
@@ -19,6 +39,7 @@ app.get('/', (req, res) => {
     res.render('places/index')
   })
   
+
 module.exports = router
 
 res.render('places/index', { places })
